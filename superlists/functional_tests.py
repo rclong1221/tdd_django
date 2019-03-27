2ce6a12ad482340b29f00b4ed112055c9224e2cd
Routhana Long <rclong1221@gmail.com>
2019-03-27 13:55:06 -0400

    first FT specced out in comments, and now uses unittest

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,33 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert "Django" in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Audrey heard about this cool new website and goes to check it out
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page header and title mention to-do lists
+        assert 'To-Do' in self.browser.title, 'Browser title was "{}"'.format(self.browser.title)
+
+        # She is invited to enter a to-do item
+
+        # She types make Dan clean the dishes
+
+        # When she hits enter, the page updates, and now the page list shows
+        # "1: make Dan clean the dishes"
+
+        # Another text box invites here to enter another to-do item
+        # She enters "make Dan clean the gutters"
+
+        # Audrey wonders if the site saved her list.  She sees a unique URL for her.
+
+        # She visits the URL and sees that her list is satisfied.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
