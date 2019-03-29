690c1d5702ac8a5ce9eba6054d91d6a23ba480ab
Routhana Long <rclong1221@gmail.com>
2019-03-28 22:37:46 -0400

    hook up superlists app, switch to template rendering, create template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.keys import Keys
+import time, unittest
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -14,17 +15,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page header and title mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item
-        self.fail("Finish the test...")
+        todo_box = self.browser.find_element_by_id('tdb')
+        self.assertEqual(
+            todo_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # She types make Dan clean the dishes
+        todo_box.send_keys('Make Dan clean the dishes')
 
         # When she hits enter, the page updates, and now the page list shows
         # "1: make Dan clean the dishes"
+        todo_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_elements_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Make Dan clean the dishes' for row in rows)
+        )
 
         # Another text box invites here to enter another to-do item
         # She enters "make Dan clean the gutters"
+        self.fail('Finish the test...')
 
         # Audrey wonders if the site saved her list.  She sees a unique URL for her.
 
