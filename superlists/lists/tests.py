dc9e95db537ab59c5d45fbd9bebcec8b5bc6ab6c
Routhana Long <rclong1221@gmail.com>
2019-03-29 21:41:48 -0400

    refactor home page test

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,14 +5,6 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do Lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
